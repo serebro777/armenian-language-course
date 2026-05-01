0955d949bcd6deba28886816f1c7b3908403e503
serebro777 <136987138+serebro777@users.noreply.github.com>
2026-05-01 22:31:01 +0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,3 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    // пусто — всё управляется из settings.gradle.kts
 }
