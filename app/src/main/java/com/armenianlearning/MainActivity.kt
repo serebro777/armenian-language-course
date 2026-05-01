bf1cfc2fb2d1715972a3f717d1342c71135f432e
serebro777 <136987138+serebro777@users.noreply.github.com>
2026-05-02 00:03:44 +0400

    Update MainActivity.kt

--- app/src/main/java/com/armenianlearning/MainActivity.kt
+++ app/src/main/java/com/armenianlearning/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
