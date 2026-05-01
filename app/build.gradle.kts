cd9cb0e8d31e16aaa2fe735b410c3db617608556
serebro777 <136987138+serebro777@users.noreply.github.com>
2026-05-01 23:41:38 +0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,12 +20,12 @@ android {
     }
 
     composeOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "17"
+         jvmTarget = "17"
     }
 }
 
